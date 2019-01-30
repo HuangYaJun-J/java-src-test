aff591a37a4600642833f8469aed7972e4da6992
huangyajun <huangyajun_j@163.com>
2019-01-30 20:01:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 用于阅读jdk源码， 并做代码注释
+
+## 1. 获取源码
+在JDK安装目录下找到源码包src.zip，解压；
+## 2. 导入idea
+新建java8-src子项目，选择新建java空白项目（非maven），然后将刚才解压的src目录替换java8-src子项目的src目录。
---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @description:
+ * @author: HuangYaJun
+ * @Email: huangyajun_j@163.com
+ * @create: 2019/1/30 14:52
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
