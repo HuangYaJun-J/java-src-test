c82324f3c629a0aec63b619f728a9c40922ccdb1
huangyajun <huangyajun_j@163.com>
2019-01-30 22:08:59 +0800

    优化代码

--- src/main/java/HYJTest.java
+++ src/main/java/HYJTest.java
@@ -2,9 +2,9 @@
  * @description:
  * @author: HuangYaJun
  * @Email: huangyajun_j@163.com
- * @create: 2019/1/30 14:52
+ * @create: 2019/1/30 22:07
  */
-public class Test {
+public class HYJTest {
     public static void main(String[] args) {
         System.out.println("hello word");
     }
